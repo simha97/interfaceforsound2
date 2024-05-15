43daf59fc2a9d9889d7eb2769eccf308b946759f
simha97 <62260118+simha97@users.noreply.github.com>
2024-05-15 21:15:27 +0200

    try2

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+
 const corsOptions = {
   origin: "https://interfaceforsound2-frontend.vercel.app",
   optionsSuccessStatus: 200,
@@ -17,15 +18,10 @@ app.use(cors(corsOptions));
 const dbURI =
   "mongodb+srv://simonhallak3:B9fQRohJNgeISs3I@soundforsleep.f573e6z.mongodb.net/?retryWrites=true&w=majority&appName=soundforsleep";
 
-mongoose.connect(dbURI);
-
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB database");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const User = require("./models/User"); // Adjust the path as necessary
 
@@ -33,12 +29,16 @@ app.post("/submit-form", async (req, res) => {
   const { name, day, relaxation, mood, selectedSound } = req.body;
 
   try {
+    console.log("Received form submission:", req.body);
+
     const user = await User.findOne({ name });
+    console.log("User found:", user);
 
     if (user) {
       // User exists, add a new entry
       user.entries.push({ day, relaxation, mood, selectedSound });
       await user.save();
+      console.log("User data updated");
       res.status(200).send("User data updated");
     } else {
       // User does not exist, create a new user
@@ -47,9 +47,11 @@ app.post("/submit-form", async (req, res) => {
         entries: [{ day, relaxation, mood, selectedSound }],
       });
       await newUser.save();
+      console.log("New user created and data added");
       res.status(201).send("New user created and data added");
     }
   } catch (error) {
+    console.error("Error saving user data:", error);
     res.status(500).send("Error saving user data: " + error.message);
   }
 });
